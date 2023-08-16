4db131c7aee143f8c450e90408bf10c27c64f62c
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-08-16 23:53:08 +0530

    Fetching details of the token

--- client/src/Pages/AdminPanel.tsx
+++ client/src/Pages/AdminPanel.tsx
@@ -1,10 +1,10 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { ethers } from "ethers";
 
 import { TokenContext } from "../Context/TokenContext";
 import PartnerRequestCard from "../Components/PartnerRequestCard";
 import PartnerCard from "../Components/PartnerCard";
-import {contractProp} from '../App'
+import { contractProp } from "../App";
 
 export default function AdminPanel({ contract }: contractProp) {
   const { partners } = useContext(TokenContext);
@@ -12,26 +12,61 @@ export default function AdminPanel({ contract }: contractProp) {
   interface tokenTypes {
     name: string;
     symbol: string;
-    decimal: number;
-    totalSupply: number;
+    decimal: bigint;
+    totalSupply: bigint;
     owner: string;
   }
 
   const [token, setToken] = useState<tokenTypes>({
     name: "",
     symbol: "",
-    decimal: 0,
-    totalSupply: 0,
+    decimal: 0n,
+    totalSupply: 0n,
     owner: "",
   });
 
-  const makePartner = async (partner: string)=>{
-    // Check is partner exists in partners array
-    contract && await contract.addAsPartner(partner);
-  }
+  const getTokenDetails = async () => {
+    const namePromise = contract?.name();
+    const symbolPromise = contract?.symbol();
+    const decimalsPromise = contract?.decimals();
+    const totalSupplyPromise = contract?.totalSupply();
+    const ownerPromise = contract?.owner();
 
-  return <div className="flex w-full gap-2 my-2">
-    <PartnerRequestCard contract={contract}/>
-    <PartnerCard contract={contract}/>
-  </div>;
+    const [name, symbol, decimal, totalSupply, owner]: [
+      string,
+      string,
+      bigint,
+      bigint,
+      string
+    ] = await Promise.all([
+      namePromise,
+      symbolPromise,
+      decimalsPromise,
+      totalSupplyPromise,
+      ownerPromise,
+    ]);
+
+    setToken({ name, symbol, decimal, totalSupply, owner });
+  };
+
+  useEffect(() => {
+    getTokenDetails();
+  }, [contract]);
+  console.log(typeof token.decimal);
+  return (
+    <>
+    {/* Improve CSS */}
+      <div className="flex justify-between">
+        <p>{token.name}</p>
+        <p>{token.symbol}</p>
+        <p>{token.decimal.toString()}</p>
+        <p>{token.totalSupply.toString()}</p>
+        <p>{token.owner}</p>
+      </div>
+      <div className="flex w-full gap-2 my-2">
+        <PartnerRequestCard contract={contract} />
+        <PartnerCard contract={contract} />
+      </div>
+    </>
+  );
 }
